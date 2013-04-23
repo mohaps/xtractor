ff2c23306f4e3feaee7ce42d882e5655b082b6bb
mohaps <mohaps@gmail.com>
2013-04-23 00:45:33 -0700

    removed double url in tweet text

--- src/main/java/com/mohaps/xtractor/XTractorServlet.java
+++ src/main/java/com/mohaps/xtractor/XTractorServlet.java
@@ -80,7 +80,7 @@ public class XTractorServlet extends HttpServlet {
 			if(tweetText.length() > 100) {
 				tweetText = tweetText.substring(0,100)+"...";
 			}
-			tweetText="\""+tweetText+"\" ("+shortUrl+") [via XTractor/@tldrzr]";
+			tweetText="\""+tweetText+"\" [via XTractor/@tldrzr]";
 			String tweetItUrl = "https://twitter.com/share?text="+URLEncoder.encode(tweetText, result.getCharset())+"&url="+URLEncoder.encode(shortUrl, result.getCharset())+"&related=tldrzr";
 			System.out.println("Tweet it url : "+tweetItUrl);
 			req.setAttribute("tweet_it_url", tweetItUrl);
